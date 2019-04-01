624a78bef8feb0af4b36e89b68c2d631da153385
Aby Thankachan <aby.thankachan@gmail.com>
2019-03-31 18:42:59 -0700

    Added code to connect to sqlite db.

--- main.go
+++ main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"text/template"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Page struct {
-	Name string
+	Name     string
+	DbStatus bool
 }
 
 func main() {
 	templates := template.Must(template.ParseFiles("templates/index.html"))
+	db, _ := sql.Open("sqlite3", "/home/aufather/dev.db")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		p := Page{Name: "Gopher"}
 		if name := r.FormValue("name"); name != "" {
 			p.Name = name
 		}
+		p.DbStatus = db.Ping() == nil
 		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
+		db.Close()
 	})
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
